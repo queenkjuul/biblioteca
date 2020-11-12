dc83ab96c58c37fac672ebdf884542fb6a5c251e
jhillTechtonic <julie.hill@techtonic.com>
2020-11-12 02:56:53 -0600

    DRYed out card styles, fixed details grid, tweaked buttons and UI FX

--- generateBooks.js
+++ generateBooks.js
@@ -1,36 +1,31 @@
 import covers from "assets/images/*.png";
-var cardCount = 20;
+var cardCount = 19;
 var title = [ "Farenheit 451", "Brave New World", "The Hitchiker's Guide to the Galaxy", "A Stitch in Time: This Book Does Not Have a Subtitle, but I Have Added One to Test Wrapping" ];
 var author = [ "Ray Bradbury", "Aldous Huxley", "Douglas Adams", "Andrew Robinson, who played Garak on the hit TV series Star Trek: Deep Space 9" ];
 
 console.log(covers);
 
-for (var i = 1; i < cardCount; i++) {
-    var nextCard = document.createElement('div');
-    var nextCover = document.createElement('img');
-    var nextTitle = document.createElement('div');
-    var nextAuthor = document.createElement('div');
+for (var i = 1; i <= cardCount; i++) {
+    var index = Math.floor(Math.random() * title.length);
+    const newCard = {
+        title: title[index],
+        author: author[index],
+        id: i,
+        cover: Object.values(covers)[index]
+    }
 
-    var index = Math.floor(Math.random() * 4);
+    createBookshelfCard(newCard);
+}
 
-    nextCard.className = "bookshelf-card";
-    nextCard.id = "bookshelf-card-" + i;
+function createBookshelfCard(card){
 
-    nextCover.className = "bookshelf-card__coverimg";
-    nextCover.id = "bookshelf-card-cover-" + i;
-    nextCover.src = Object.values(covers)[index];
-    nextCover.alt = title[index];
-    
+    const cardHTML = `
 
-    nextTitle.innerHTML = title[index];
-    nextTitle.className = "bookshelf-card__title";
-
-    nextAuthor.innerHTML = author[index];
-    nextAuthor.className = "bookshelf-card__author";
-
-
-    document.getElementById("bookshelf__container").appendChild(nextCard);
-    document.getElementById(nextCard.id).appendChild(nextCover);
-    document.getElementById(nextCard.id).appendChild(nextTitle);
-    document.getElementById(nextCard.id).appendChild(nextAuthor);
+        <a href="details.html" class="card--bookshelf" id="card--bookshelf-${card.id}">
+            <img class="card--bookshelf__coverimg" src="${card.cover}" alt="Cover of the book ${card.title}">
+            <div class="card--bookshelf__title">${card.title}</div>
+            <div class="card--bookshelf__author">${card.author}</div>
+        </a>
+`
+    document.getElementById('bookshelf__container').insertAdjacentHTML('beforeend', cardHTML);
 }
