60b665f74459464ea8dd4d737a485f645c0b49ef
jhillTechtonic <72571571+jhillTechtonic@users.noreply.github.com>
2020-12-09 21:13:05 +0000

    Feedback refactor (#3)
    * fixed indentation on details.html
    
    * fixed margin for --pageCount in _book-form.scss
    
    * Committing changes requested by drogersTechtonic
    
    * fixed closing tags in edit.html and addbook.html textareas

--- generateBooks.js
+++ generateBooks.js
@@ -3,8 +3,6 @@ var cardCount = 19;
 var title = [ "Farenheit 451", "Brave New World", "The Hitchiker's Guide to the Galaxy", "A Stitch in Time: This Book Does Not Have a Subtitle, but I Have Added One to Test Wrapping" ];
 var author = [ "Ray Bradbury", "Aldous Huxley", "Douglas Adams", "Andrew Robinson, who played Garak on the hit TV series Star Trek: Deep Space 9" ];
 
-console.log(covers);
-
 for (var i = 1; i <= cardCount; i++) {
     var index = Math.floor(Math.random() * title.length);
     const newCard = {
