0bbf1296e9ca25cb657ab3ea832043c5e0fc84aa
jhillTechtonic <julie.hill@techtonic.com>
2021-01-18 11:19:33 -0600

    VLP-1590-set up sequelize models and associations, set up sync in server.js

---
+++ server/models/book.js
@@ -0,0 +1,59 @@
+const { DataTypes } = require("sequelize")
+
+module.exports = (sequelize, datatypes) => {
+    const Book = sequelize.define('Book', {
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [1, 100],
+            },
+        },
+        synopsis: {
+            type: DataTypes.TEXT,
+            allowNull: true,
+            validate: {
+                len: [0, 5000],
+            },
+        },
+        pageCount: {
+            type: DataTypes.INTEGER,
+            allowNull: true,
+            validate: {
+                isNumeric: true,
+                min: 0,
+                max: 2000,
+            },
+        },
+        rating: {
+            type: DataTypes.INTEGER,
+            allowNull: true,
+            validate: {
+                isNumeric: true,
+                min: 1,
+                max: 5,
+            },
+        },
+        publishDate: {
+            type: DataTypes.DATEONLY,
+            allowNull: true,
+        },
+        coverimg: {
+            type: DataTypes.STRING,
+            allowNull: true,
+            validate: {
+                len: [0, 2000],
+            },
+        },
+    },
+    {
+        paranoid: true,
+        timestamps: true,
+    });
+
+    Book.associate = models => {
+        Book.hasOne(models.Author);
+    }
+
+    return Book;
+}
--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const logger = require('morgan');
 const cors = require('cors');
+const { sequelize } = require('./models');
 const app = express();
 const PORT = process.env.PORT || 8080;
 
@@ -23,6 +24,8 @@ app.get('/', (req, res) => {
     res.send('GET request from root route');
 })
 
-app.listen(PORT, () => {
-    console.log(`API server listening on http://localhost:${PORT}!`);
+sequelize.sync().then(() => {
+    app.listen(PORT, () => {
+        console.log(`API server listening on http://localhost:${PORT}!`);
+    });
 });
