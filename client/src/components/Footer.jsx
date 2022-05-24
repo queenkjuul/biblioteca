26a4dd9495f83bd5d721b3f54f78fbdb49c2da72
queenkjuul <queenkjuul@pm.me>
2022-05-24 02:51:48 -0500

    update footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -6,6 +6,7 @@ const Footer = () => {
             © 2020 Her Majesty Queen Julia I<br></br>
             <span class="notes">
                 Created by Julie Hill @ julie dot hill at pm.me< br />
+                Try it on mobile!<br />
                 <a href="https://www.linkedin.com/in/julie-hill-3099811b9/">LinkedIn</a> |
                 <a href="https://github.com/queenkjuul">  Github</a></span><br />
             <span class="details">This site is hosted on a DO Droplet running Ubuntu 22.04 and nginx, set up from scratch</span>
