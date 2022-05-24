18df7ff1531db1d2c0b157f2289a3fd592dc1e52
queenkjuul <queenkjuul@pm.me>
2022-05-24 02:07:31 -0500

    update readme and footer and package.json

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -3,7 +3,13 @@ import React from 'react';
 const Footer = () => {
     return (
         <footer className="footer">
-                © 2020 Her Majesty Queen Julia I
+            © 2020 Her Majesty Queen Julia I<br></br>
+            <span class="notes">
+                Created by Julie Hill @ julie dot hill at pm.me< br />
+                <a href="https://www.linkedin.com/in/julie-hill-3099811b9/">LinkedIn</a> |
+                <a href="https://github.com/queenkjuul">  Github</a></span><br />
+            <span class="details">This site is hosted on a DO Droplet running Ubuntu 22.04 and nginx, set up from scratch</span>
+            <br></br>
         </footer>
     )
 }
