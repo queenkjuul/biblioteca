da67e2956322382fca9433f30d3936528c2992ed
queenkjuul <queenkjuul69@gmail.com>
2024-04-03 04:09:09 -0500

    240403: trying to disable CORS server side

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ const env = process.env.NODE_ENV || "development"
 const config = require(__dirname + "/config/config.js")[env]
 
 const corsOptions = {
-  origin: config.origin,
+  origin: "*",
 }
 
 app.use(logger("dev"))
